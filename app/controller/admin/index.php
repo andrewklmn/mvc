8ea2845f3b78319efcfdb66208e5c3f15bfc1375
user <user@localhost.localdomain>
2015-05-16 23:12:17 +0300

    очистил от всего костыльного для последующих экзерсисов...

--- app/controller/admin/index.php
+++ app/controller/admin/index.php
@@ -1,36 +1,22 @@
 <?php
 
-/*
- * Default Controller
- */
-
     // Check run constant
     if(!isset($c)) exit;
     
-    include_once 'app/model/db/config.php';
-    include_once 'app/model/db/db.php';
-    include_once 'app/view/draw_table_from_sql.php';
-        
-    $data['title'] = 'Objects';
+    include 'app/view/header_utf-8.php';
     include 'app/view/html_head.php';
     
-    include 'app/model/admin/menu.php';
-    include 'app/view/menu.php';
-    
-   
-  
 ?>
-<script>
-
-</script>
 <div class="container">
-    <h3>Объекты под сдачу</h3>
-    <?php
-        include 'app/view/admin/objects.php';
-    ?>
+    <h1>
+        Index
+    </h1>
+    <p>
+        <?php
+            pre($_SESSION[$program]);
+        ?>
+    </p>
+    <a class="link" href="?c=logout">Sign out</a>
 </div>
-    <?php
-        include 'app/view/admin/objects_buttons.php';
-    ?>
 </body>
-</html>                
+</html>
--- app/view/table_form/activate_make_form.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    if (isset($_GET['action']) AND $_GET['action']='make_form') {
-        ?>
-            <input type="hidden" name="code" value="<?php echo $code; ?>"/>
-            <input type="hidden" name="make_form_action" value="Preview"/>
-        <?php
-    };      
-?>
